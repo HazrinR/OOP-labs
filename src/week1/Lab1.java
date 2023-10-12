4604b90029cf3dc19636def129383631b941546c
HazrinR <hazrin.redzepi@treca-gimnazija.edu.ba>
2023-10-12 15:31:39 +0200

    week 01 commitan

---
+++ src/week1/Lab1.java
@@ -0,0 +1,67 @@
+package week1;
+
+import java.util.Scanner;
+
+
+public class Lab1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hazrin Redzepi");
+    }
+}/*
+public class Lab1{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!\n(And all the people of the world)");
+    }
+}
+public class Lab1 {
+    public static void main(String[] args){
+    int seconds=365*24*60*60;
+        System.out.println("There are "+ seconds + " seconds in a year.");
+}
+}
+public class Lab1{
+    public static void main(String[] args)
+    {
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Enter first number ");
+        int firstNumber=Integer.parseInt(reader.nextLine());
+        System.out.println("Enter second number ");
+        int secondNumber=Integer.parseInt(reader.nextLine());
+        int sum=firstNumber+secondNumber;
+        System.out.println("Sum of the numbers "+ sum);
+
+    }
+
+public class Lab1{
+    public static void main(String[] args)
+    {
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Type a number ");
+        int number=Integer.parseInt(reader.nextLine());
+        if (number>0){
+            System.out.println("The number is positive.");
+        }else {
+            System.out.println("The number is not positive.");
+        }
+
+    }}
+public class Lab1{
+    public static void main(String[] args)
+    {
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Enter first number ");
+        int firstNumber=Integer.parseInt(reader.nextLine());
+        System.out.println("Enter second number ");
+        int secondNumber=Integer.parseInt(reader.nextLine());
+        if (firstNumber==secondNumber)
+        {
+            System.out.println("The Numbers are equal!");
+        } else if (firstNumber>secondNumber) {
+            System.out.println("First number is greater " +firstNumber);
+        }else {
+            System.out.println("The second number is greater " +secondNumber);
+        }
+
+    }}*/
